55da9389459a9f181ae2e68de60d900526c996a1
cinnamoneight26 <cinnamon.eight26@gmail.com>
2022-11-30 14:45:10 +0900

    오타 수정

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -8,10 +8,10 @@
 </template>
 
 <script>
-import TodoHeader from "./conponents/TodoHeader.vue";
-import TodoInput from "./conponents/TodoInput.vue";
-import TodoList from "./conponents/TodoList.vue";
-import TodoFooter from "./conponents/TodoFooter.vue";
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoList from "./components/TodoList.vue";
+import TodoFooter from "./components/TodoFooter.vue";
 
 export default {
   components: {
@@ -29,13 +29,16 @@ body {
   text-align: center;
   background-color: #f6f6f6;
 }
+
 input {
   border-style: groove;
   width: 200px;
 }
+
 button {
   border-style: groove;
 }
+
 .shadow {
   box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
 }
